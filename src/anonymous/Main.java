7a61b540eebdcab4b9fe51c1fca75a476d85b34d
Irza Hasan <irzahasan07@gmail.com>
2025-06-20 08:07:02 +0500

    conversion of anonymous class

--- src/anonymous/Main.java
+++ src/anonymous/Main.java
@@ -1,5 +1,13 @@
 package anonymous;
 
+import java.text.RuleBasedCollator;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
         Messenger messenger = new Messenger() {
@@ -30,5 +38,59 @@ public class Main {
                 System.out.println("Sending Email: "+message);
             };
         });
+
+
+        MyInterface myInterface1 =(int value) -> System.out.println("Hello "+value);
+
+        myInterface1.printValues(7);
+        myInterface1.testing();
+        myInterface1.testing();
+        myInterface1.testing();
+
+        MyInterface myInterface = (int value) ->{
+                for (int i =0; i<5; i++){
+                    int b = i;
+                    Runnable runnable = ()-> System.out.println("Hello "+b);
+                    new Thread(runnable).start();
+                }
+        };
+
+
+        myInterface.printValues(7);
+        myInterface.toString();
+
+
+        System.out.println();
+        System.out.println("=== Built in Interfaces implementation ===");
+        System.out.println();
+        System.out.println("---Consumer Interface");
+
+        Consumer<String> consumer = (name)->{
+            System.out.println("Assalam u Alaiakum. My name is "+ name);
+        };
+        consumer.accept("Irza");
+
+        System.out.println();
+        System.out.println("---BiFunction Interface");
+        BiFunction<Integer , String , String> biFunction = (value, name)->{
+            return (value + " "+ name);
+        };
+
+        System.out.println(biFunction.apply(37, "Irziii Pirziii"));
+
+        System.out.println();
+        System.out.println("---ForEach loop in list");
+
+        List<String> myList = Arrays.asList("aaa","aab", "aba", "abb","baa","bab","bba","bbb");
+        myList.forEach(System.out::println);
+        System.out.println();
+        System.out.println("---Streams API");
+
+        myList.stream().filter(name -> name.startsWith("a")).unordered().forEach(System.out :: println);
+
+        myList.sort(( o1,  o2)-> o1.compareTo(o2));
+        myList.sort(String::compareTo);
+
+
     }
 }
